300cfe23febf595984b269e04ea23095026e54da
10xdev4u-alt <10xdev4u@gmail.com>
2026-01-19 23:01:36 +0530

    feat: implement message handling and gossip logic

--- pkg/gossip/gossiper.go
+++ pkg/gossip/gossiper.go
@@ -1,6 +1,9 @@
 package gossip
 
 import (
+	"encoding/json"
+	"log"
+	"math/rand"
 	"time"
 )
 
@@ -23,11 +26,23 @@ func NewGossiper(transport Transport) *Gossiper {
 // Start starts the gossip loop.
 func (g *Gossiper) Start() {
 	go g.gossipLoop()
+	go g.listen()
 }
 
 // Stop stops the gossip loop.
 func (g *Gossiper) Stop() {
 	close(g.stop)
+}
+
+func (g *Gossiper) listen() {
+	for {
+		select {
+		case data := <-g.transport.Read():
+			g.handleMessage(data)
+		case <-g.stop:
+			return
+		}
+	}
 }
 
 func (g *Gossiper) gossipLoop() {
@@ -45,6 +60,55 @@ func (g *Gossiper) gossipLoop() {
 }
 
 func (g *Gossiper) gossip() {
-	// For now, we don't have any nodes to gossip to.
-	// This will be implemented later.
+	nodes := g.members.All()
+	if len(nodes) == 0 {
+		return
+	}
+
+	// Select a random node to gossip to.
+	node := nodes[rand.Intn(len(nodes))]
+
+	// Create a ping message with the membership list.
+	payload, err := json.Marshal(g.members.All())
+	if err != nil {
+		log.Printf("failed to marshal membership list: %v", err)
+		return
+	}
+
+	msg := &Message{
+		Type:    Ping,
+		Payload: payload,
+	}
+
+	// Encode the message.
+	data, err := msg.Encode()
+	if err != nil {
+		log.Printf("failed to encode message: %v", err)
+		return
+	}
+
+	// Send the message.
+	if err := g.transport.Write(data, node.Addr.String()); err != nil {
+		log.Printf("failed to send message to %s: %v", node.Addr.String(), err)
+	}
+}
+
+func (g *Gossiper) handleMessage(data []byte) {
+	msg, err := Decode(data)
+	if err != nil {
+		log.Printf("failed to decode message: %v", err)
+		return
+	}
+
+	switch msg.Type {
+	case Ping:
+		var nodes []*Node
+		if err := json.Unmarshal(msg.Payload, &nodes); err != nil {
+			log.Printf("failed to unmarshal ping payload: %v", err)
+			return
+		}
+		for _, node := range nodes {
+			g.members.Add(node)
+		}
+	}
 }
